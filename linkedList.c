df272c47ed5aad76ec0b70d8407b3f5cbe74925d
choihj0202 <helenchoi22@naver.com>
2018-12-06 15:23:28 +0900

    exercise1

---
+++ linkedList.c
@@ -0,0 +1,12 @@
+
+typedef struct linknd{
+	int data;	// 데이터 저장 변수 선언 	
+	
+	void *next;	// 다음 node를 가리킬 포인터 변수 선언  
+	//struct linknd *next;
+} linknd_t;	// typedef 조합 
+
+
+
+//struct linknd nd;
+//linknd_t nd;  
