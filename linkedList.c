f8e099ac077ad56fcf2d5a41136f03d3b72d625b
choihj0202 <helenchoi22@naver.com>
2018-12-06 15:57:54 +0900

    exercise3

--- linkedList.c
+++ linkedList.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+// node 형태 구현
 typedef struct linknd {
+	
 	int data;	// 데이터 저장 멤버 
 	
 	void *next;	// 다음 node를 가리킬 포인터 변수 선언  
 	//struct linknd *next;
-} linknd_t;	// typedef 조합, node 형태 구현  
+} linknd_t;	// typedef 조합   
 
-//정적 할당, 외부에서 접근 불가, static 없으면 다른 파일에서 extern해도 접근 불가   
-static linknd_t *list;	// linked list 실체 구현, struct 빠짐, typedef 써서  
+//정적 할당, 외부에서 접근 불가, static 없으면 다른 파일에서 extern해도 접근 불가 
+// linked list 실체 구현
+static linknd_t *list;	// struct 빠짐, typedef 써서  
 
+// node 생성 함수 구현  
 linknd_t* create_node(int value) {
 	
-	linknd_t* ndPtr;
+	linknd_t *ndPtr;
 	
 	//동적 메모리 할당 
 	ndPtr = (linknd_t*)malloc( sizeof(linknd_t) );
@@ -29,5 +33,28 @@ linknd_t* create_node(int value) {
 	ndPtr->next - NULL;	// linked list의 next 초기화 
 	
 	return ndPtr; 
+}
+
+// node 연결
+void addTail(int value) {
+	
+	linknd_t *ndPtr, *newPtr;
+	
+	if (list == NULL)
+	{
+		return;
+	}
+	
+	else
+	{
+		ndPtr = list;
+		while (ndPtr->next != NULL)
+		{
+			ndPtr = ndPtr->next;
+		}
+		
+		newPtr = create_node(value);
+		ndPtr->next = newPtr;
+	}
 }
 
